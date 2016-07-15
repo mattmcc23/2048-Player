e4e10584e7f0ca581edd7bfcb4aa9e2c79824bff
Matthew McClure <matt.mcc@utexas.edu>
2016-07-15 18:36:51 -0500

    Added command line argument functionality

--- 2048Player.py
+++ 2048Player.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+import sys
 
 browser = webdriver.Firefox()
 browser.get("https://gabrielecirulli.github.io/2048/")
@@ -10,15 +10,26 @@ scoreText = browser.find_element_by_class_name('score-container')
 message = browser.find_element_by_class_name('game-message')
 retry = browser.find_element_by_class_name('retry-button')
 
-numGames = 10
+numGames = 3
 currentGame = 0
+
+topScore = -1
+
+if len(sys.argv) > 1:
+	numGames = int(sys.argv[-1])
+
+print('Playing ' + str(numGames) + ' games of 2048:')
 
 while currentGame < numGames:
 	while message.is_displayed() == False:
 		game.send_keys('wasd')
 	currentGame += 1
 	scoreMessage = scoreText.text.split('\n')
-	score = scoreMessage[0]
+	score = int(scoreMessage[0])
+	if score > topScore:
+		topScore = score
 	print('Game ' + str(currentGame) + ': ' +  str(score) + ' points')
 	retry.click()
 
+if numGames > 1:
+	print('Top Score: ' + str(topScore) + ' points')
